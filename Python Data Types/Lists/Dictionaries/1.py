7fadb6d7879231233f9bf41a6fdf478a66770e92
Sheela Khatun <110275046+sheela-ml@users.noreply.github.com>
2025-10-24 14:11:41 +0000

    updates

---
+++ Python Data Types/Lists/Dictionaries/1.py
@@ -0,0 +1,11 @@
+# Add and Update.py
+# Demonstrate adding and updating dictionary entries.
+
+data = {"a": 1, "b": 2}
+
+data["c"] = 3
+data["a"] = 10
+
+if __name__ == '__main__':
+    print("Updated dictionary:", data)
+
